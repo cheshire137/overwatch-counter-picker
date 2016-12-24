65f49bd3a78df0846e42fed5076427ca9bd5fb69
Sarah Vessels <cheshire137@gmail.com>
2016-12-24 14:51:07 -0600

    Add homepage template with Bulma CSS

--- src/server.py
+++ src/server.py
@@ -1,18 +1,10 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-  the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-  return """
-  <h1>Hello heroku</h1>
-  <p>It is currently {time}.</p>
-
-  <img src="http://loremflickr.com/600/400">
-  """.format(time=the_time)
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
