1193ecd6d2edc96208f981c18b7c25abd1936334
Sarah Vessels <cheshire137@gmail.com>
2016-12-23 20:10:26 -0600

    Detect multiple Mercys
    Only finding the first one, on the enemy team. Better template needed?
    
    http://docs.opencv.org/trunk/d4/dc6/tutorial_py_template_matching.html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import cv2
+import numpy as np
 
-original = cv2.imread('sample-screenshots/full-teams.jpg', 0)
+original = cv2.imread('sample-screenshots/full-teams.jpg')
+original_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+
 template = cv2.imread('heroes/mercy.png', 0)
 w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(original, template, cv2.TM_CCORR)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-top_left = max_loc
-bottom_right = (top_left[0] + w, top_left[1] + h)
+res = cv2.matchTemplate(original_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+  cv2.rectangle(original, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+cv2.imwrite('res.png', original)
 
-print 'Found Mercy at', top_left, 'to', bottom_right
+print 'Look at res.png to see Mercy detection'
