c23c970f022f460f599000d0e0c160b46e9e54b3
Sarah Vessels <cheshire137@gmail.com>
2016-12-23 20:24:10 -0600

    Detect each hero we have a template for
    Also use variables to make it clear what each rectangle parameter is.
    
    http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.html

--- app.py
+++ app.py
@@ -4,14 +4,24 @@ import numpy as np
 original = cv2.imread('sample-screenshots/full-teams.jpg')
 original_gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
-template = cv2.imread('heroes/mercy.png', 0)
-w, h = template.shape[::-1]
+heroes = ['dva', 'genji', 'lucio', 'mercy', 'reaper', 'reinhardt',
+          'roadhog', 'tracer', 'zarya', 'zenyatta']
 
-res = cv2.matchTemplate(original_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):
-  cv2.rectangle(original, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-cv2.imwrite('res.png', original)
+for hero in heroes:
+  print 'Detecting', hero, '...'
+  template = cv2.imread('heroes/' + hero + '.png', 0)
+  w, h = template.shape[::-1]
 
-print 'Look at res.png to see Mercy detection'
+  res = cv2.matchTemplate(original_gray, template, cv2.TM_CCOEFF_NORMED)
+  threshold = 0.8
+  loc = np.where(res >= threshold)
+  thickness = 2
+  for pt in zip(*loc[::-1]):
+    point1 = pt
+    point2 = (pt[0] + w, pt[1] + h)
+    color = (0, 0, 255)
+    cv2.rectangle(original, point1, point2, color, thickness)
+
+output_path = 'res.png'
+cv2.imwrite(output_path, original)
+print 'Look at', output_path, 'to see Overwatch hero detection'
