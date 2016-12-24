2c414fbafe3eb769463c6e24155b3f76e6a924f0
Sarah Vessels <cheshire137@gmail.com>
2016-12-24 12:16:57 -0600

    Add test for healer when starting match

--- test/hero_picker_test.py
+++ test/hero_picker_test.py
@@ -2,20 +2,26 @@ import unittest
 from src.hero_picker import HeroPicker
 
 class HeroPickerTest(unittest.TestCase):
-  def test_suggests_healer(self):
+  def test_suggests_counter_healer(self):
     red_team = ['mercy', 'lucio', 'dva', 'genji', 'tracer', 'reinhardt']
     blue_team = ['dva', 'genji', 'hanzo', 'widowmaker', 'mccree']
     hero_picker = HeroPicker(red_team, blue_team)
     self.assertEqual(['lucio'], hero_picker.pick())
 
-  def test_suggests_tank(self):
+  def test_suggests_counter_tank(self):
     red_team = ['hanzo', 'lucio', 'reaper', 'genji', 'tracer', 'reinhardt']
     blue_team = ['mercy', 'genji', 'hanzo', 'widowmaker', 'mccree']
     hero_picker = HeroPicker(red_team, blue_team)
     self.assertEqual(['dva'], hero_picker.pick())
 
-  def test_suggests_dps(self):
+  def test_suggests_counter_offense(self):
     red_team = ['hanzo', 'lucio', 'reaper', 'genji', 'mei', 'reinhardt']
     blue_team = ['mercy', 'reinhardt', 'hanzo', 'widowmaker', 'bastion']
     hero_picker = HeroPicker(red_team, blue_team)
     self.assertEqual(['pharah', 'reaper'], hero_picker.pick())
+
+  def test_suggests_healer(self):
+    red_team = []
+    blue_team = ['dva', 'roadhog', 'reinhardt', 'genji', 'mei']
+    hero_picker = HeroPicker(red_team, blue_team)
+    self.assertEqual(['mercy', 'ana'], hero_picker.pick())
