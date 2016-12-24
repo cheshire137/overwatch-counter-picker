9b9d27c7fabbe4f84b936e59f8e4d396da9ea0ec
Sarah Vessels <cheshire137@gmail.com>
2016-12-23 23:57:37 -0600

    Just work with TeamDetector

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import sys
 original_name = 'full-teams.jpg'
 if len(sys.argv) > 1:
   original_name = sys.argv[1]
-  print 'Using screenshot', original_name
+  print 'Using screenshot', original_name, '\n'
 else:
   print 'You can pass a file name from sample-screenshots/'
   print 'Example:'
   print 'python', sys.argv[0], 'name_of_sample_screenshot_here\n'
-  print 'Using default screenshot', original_name
+  print 'Using default screenshot', original_name, '\n'
 
 class HeroDetector:
   def __init__(self, original):
@@ -20,11 +20,13 @@ class HeroDetector:
     self.original_h = np.size(self.original, 0)
     self.mid_height = self.original_h / 2
     self.threshold = 0.8
+    self.thickness = 2
+    self.color = (0, 0, 255)
 
-  def draw_divider(self, color, thickness):
+  def draw_divider(self):
     point1 = (0, self.mid_height)
     point2 = (self.original_w, self.mid_height)
-    cv2.rectangle(self.original, point1, point2, color, thickness)
+    cv2.rectangle(self.original, point1, point2, self.color, self.thickness)
 
   def detect(self, template):
     res = cv2.matchTemplate(self.original_gray, template, cv2.TM_CCOEFF_NORMED)
@@ -40,12 +42,17 @@ class TeamDetector:
             'mercy', 'reaper', 'reinhardt', 'roadhog', 'soldier-76',
             'torbjorn', 'tracer', 'widowmaker', 'zarya', 'zenyatta']
 
-  def __init__(self, hero_detector):
+  def __init__(self, original):
     self.red_team = set()
     self.blue_team = set()
-    self.hero_detector = hero_detector
+    self.original = original
+    self.thickness = 2
+    self.color = (255, 0, 0)
+    self.hero_detector = HeroDetector(self.original)
 
-  def detect(self, color, thickness):
+  def detect(self):
+    self.hero_detector.draw_divider()
+
     for hero in self.__class__.heroes:
       template = cv2.imread('heroes/' + hero + '.png', 0)
       w, h = template.shape[::-1]
@@ -57,21 +64,15 @@ class TeamDetector:
         else:
           self.blue_team.add(hero)
         point2 = (point1[0] + w, point1[1] + h)
-        cv2.rectangle(self.hero_detector.original, point1, point2, color, thickness)
-
-original = cv2.imread('sample-screenshots/' + original_name)
-hero_detector = HeroDetector(original)
-team_detector = TeamDetector(hero_detector)
+        cv2.rectangle(self.original, point1, point2, self.color, self.thickness)
 
 thickness = 2
 blue = (255, 0, 0)
 red = (0, 0, 255)
 
-print 'Screenshot is', hero_detector.original_w, 'x', hero_detector.original_h
-print 'Vertical midpoint is', hero_detector.mid_height, '\n'
-
-hero_detector.draw_divider(blue, thickness)
-team_detector.detect(red, thickness)
+original = cv2.imread('sample-screenshots/' + original_name)
+team_detector = TeamDetector(original)
+team_detector.detect()
 
 print 'Red team:', team_detector.red_team
 print 'Blue team:', team_detector.blue_team
