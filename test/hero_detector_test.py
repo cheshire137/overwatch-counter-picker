6284f7ce661235df3b8dcda459be007fd0aca553
Sarah Vessels <cheshire137@gmail.com>
2016-12-25 12:20:16 -0600

    Add is_red_team test

--- test/hero_detector_test.py
+++ test/hero_detector_test.py
@@ -9,3 +9,9 @@ class HeroDetectorTest(unittest.TestCase):
     self.assertEqual(2560, detector.original_w)
     self.assertEqual(1440, detector.original_h)
     self.assertEqual(720, detector.mid_height)
+
+  def test_is_red_team(self):
+    original = cv2.imread('sample-screenshots/full-teams.jpg')
+    detector = HeroDetector(original)
+    self.assertTrue(detector.is_red_team(400))
+    self.assertFalse(detector.is_red_team(900))
