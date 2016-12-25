beec43047d72e7fa9df8629088650bf3f686ffa2
Sarah Vessels <cheshire137@gmail.com>
2016-12-25 16:06:38 -0600

    Add failing test for issue #1
    Illustrates #1.

---
+++ test/team_detector_test.py
@@ -0,0 +1,19 @@
+import unittest
+import cv2
+
+from src.models.team_detector import TeamDetector
+
+class TeamDetectorTest(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    cls.smaller_screenshot = cv2.imread('sample-screenshots/smaller-screenshot.jpg')
+
+  def test_detect_populates_teams(self):
+    detector = TeamDetector(self.__class__.smaller_screenshot)
+    self.assertEqual([], detector.red_team.heroes)
+    self.assertEqual([], detector.blue_team.heroes)
+    detector.detect()
+    self.assertEqual(['mccree', 'ana', 'zarya', 'dva', 'widowmaker', \
+                      'zenyatta'], detector.red_team.heroes)
+    self.assertEqual(['zenyatta', 'mercy', 'dva', 'soldier-76', 'torbjorn', \
+                      'reinhardt'], detector.blue_team.heroes)
