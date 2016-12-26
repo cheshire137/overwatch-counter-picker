9f3065fc4156ef710debb4214c813bf33221a262
Sarah Vessels <cheshire137@gmail.com>
2016-12-26 13:25:53 -0600

    Style the upload field a bit
    https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/

--- src/web/static/index.js
+++ src/web/static/index.js
@@ -1,24 +1,38 @@
 (function() {
-  const input = document.getElementById('file')
+  const fileInput = document.getElementById('file')
+  const fileLabel = document.getElementById('file-input-label')
   const sampleScreenshot = document.getElementById('sample-screenshot-container')
   const previewContainer = document.getElementById('screenshot-preview-container')
   const preview = document.getElementById('screenshot-preview')
   const button = document.getElementById('submit-button')
 
-  input.addEventListener('change', function() {
+  function fileSelected(file, event) {
+    button.disabled = false
+
+    const reader = new FileReader()
+    reader.addEventListener('load', function(event) {
+      preview.src = event.target.result
+      sampleScreenshot.style.display = 'none'
+      previewContainer.style.display = 'block'
+    })
+    reader.readAsDataURL(file)
+
+    fileLabel.setAttribute('data-original', fileLabel.textContent.trim())
+    fileLabel.textContent = event.target.value.split('\\').pop()
+  }
+
+  function noFileSelected() {
+    button.disabled = true
+    previewContainer.style.display = 'none'
+    sampleScreenshot.style.display = 'block'
+    fileLabel.textContent = fileLabel.getAttribute('data-original')
+  }
+
+  fileInput.addEventListener('change', function(event) {
     if (this.files && this.files[0]) {
-      button.disabled = false
-      const reader = new FileReader()
-      reader.addEventListener('load', function(event) {
-        preview.src = event.target.result
-        sampleScreenshot.style.display = 'none'
-        previewContainer.style.display = 'block'
-      })
-      reader.readAsDataURL(this.files[0])
+      fileSelected(this.files[0], event)
     } else {
-      button.disabled = true
-      previewContainer.style.display = 'none'
-      sampleScreenshot.style.display = 'block'
+      noFileSelected()
     }
   })
 
