41dac7c9946f586f08ec664d305109201ff23243
Sarah Vessels <cheshire137@gmail.com>
2017-01-16 15:20:24 -0600

    Make static assets include modification time in URL
    So JS and CSS won't get cached always.

--- src/web/server.py
+++ src/web/server.py
@@ -115,6 +115,33 @@ def get_pick_page_count():
   total_rows = Pick.query.count()
   return int(math.ceil(total_rows / float(STATS_PER_PAGE)))
 
+def static_file_hash(filename):
+  return int(os.stat(filename).st_mtime)
+
+
+###########################################################################
+# App config ##############################################################
+###########################################################################
+
+@app.url_defaults
+def hashed_url_for_static_file(endpoint, values):
+  if 'static' == endpoint or endpoint.endswith('.static'):
+    filename = values.get('filename')
+    if filename:
+      if '.' in endpoint: # has higher priority
+        blueprint = endpoint.rsplit('.', 1)[0]
+      else:
+        blueprint = request.blueprint # can be None too
+
+      if blueprint:
+        static_folder = app.blueprints[blueprint].static_folder
+      else:
+        static_folder = app.static_folder
+
+      param_name = 'h'
+      while param_name in values:
+        param_name = '_' + param_name
+      values[param_name] = static_file_hash(os.path.join(static_folder, filename))
 
 ###########################################################################
 # Routes ##################################################################
