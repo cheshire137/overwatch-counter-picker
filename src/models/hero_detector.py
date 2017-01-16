87635459343d368ea499ddffb7a424b1da3db5a3
Sarah Vessels <cheshire137@gmail.com>
2017-01-16 12:24:34 -0600

    Make combine_points, round class methods

--- src/models/hero_detector.py
+++ src/models/hero_detector.py
@@ -28,7 +28,7 @@ class HeroDetector:
     points = zip(*loc[::-1])
 
     if len(points) > 0:
-      return self.combine_points(points)
+      return HeroDetector.combine_points(points)
 
     return None
 
@@ -36,36 +36,48 @@ class HeroDetector:
   def is_red_team(self, point):
     return point < self.mid_height
 
-  def combine_points(self, points):
-    rounded_points = [(self.round(point[0]), self.round(point[1])) for point in points]
+  # Given a list of points where a particular hero was detected in the larger
+  # image, this will combine the points that are close enough together that they
+  # must be detecting the same hero.
+  @classmethod
+  def combine_points(cls, points):
+    rounded_points = [(cls.round(point[0]), cls.round(point[1])) for point in points]
     unique_points = list(set(rounded_points))
 
-    x_filter = lambda p, point: p != point and p[1] == point[1] and \
-      abs(point[0] - p[0]) <= 40
-    combined_points = self.combine_similar_points(unique_points, x_filter)
+    x_filter = lambda p, point: p[1] == point[1] and abs(point[0] - p[0]) <= 40
+    combined_points = cls.combine_similar_points(unique_points, x_filter)
 
-    y_filter = lambda p, point: p != point and p[0] == point[0] and \
-      abs(point[1] - p[1]) <= 40
-    combined_points = self.combine_similar_points(combined_points, y_filter)
+    y_filter = lambda p, point: p[0] == point[0] and abs(point[1] - p[1]) <= 40
+    combined_points = cls.combine_similar_points(combined_points, y_filter)
 
     return combined_points
 
-  def combine_similar_points(self, points, filterer):
-    combined_points = []
+  # Given a list of points and a lambda filter function, this will return a subset
+  # of those points after using the filter to find similar points and average
+  # them together.
+  @classmethod
+  def combine_similar_points(cls, points, filterer):
+    result = []
+
     for point in points:
-      similar_points = filter(lambda p: filterer(p, point), points)
+      similar_points = filter(lambda p: p != point and filterer(p, point), points)
+
       if len(similar_points) < 1:
-        combined_points.append(point)
+        result.append(point)
         continue
+
       similar_point = similar_points[0]
       avg_x = int((point[0] + similar_point[0]) / 2.0)
       avg_y = int((point[1] + similar_point[1]) / 2.0)
+
       avg_point = (avg_x, avg_y)
-      if not avg_point in combined_points:
-        combined_points.append(avg_point)
-    return combined_points
+      if not avg_point in result:
+        result.append(avg_point)
+
+    return result
 
   # Rounds a point so we don't detect the same hero because we matched the template
   # starting at several different-but-very-similar top-left points.
-  def round(self, num):
+  @classmethod
+  def round(cls, num):
     return int(math.floor(num / 40.0)) * 40
